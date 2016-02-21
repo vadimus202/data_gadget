faa6346819c9786a792a7bc94908b79799eabd7b
vadimus202 <vadimus202@gmail.com>
2016-02-20 20:41:56 -0500

    fix plot overlaps

--- code/02a_ui.R
+++ code/02a_ui.R
@@ -1,13 +1,17 @@
 ui <- 
     miniPage(
+        
         gadgetTitleBar("Data Variables" %>% paste(dim(df) %>% paste(collapse = ' x '))),
+        
         miniTabstripPanel(
+            # Numeric Tab
             miniTabPanel("Numeric", icon = icon("list-ol"),
                          fillRow(
                              DT::dataTableOutput("table_num", height = "100%"), 
                              fillCol(
                                  plotOutput("plot_hist", height = "100%"),
-                                 plotOutput("plot_hist_log", height = "100%")
+                                 plotOutput("plot_hist_log", height = "100%"),
+                                 flex = 1, width = "100%", height = "90%"
                              ),
                              flex = c(3, 2), width = "100%", height = "100%"),
                          miniButtonBlock(
@@ -15,8 +19,11 @@ ui <-
                          )
                          
             ),
+            
+            # Text Variables Tab
             miniTabPanel("Character", icon = icon("sort-alpha-asc"),
                          DT::dataTableOutput("table_char")
             )
-        )
-    )
+            
+        ) # end strip
+    ) # end miniPage
